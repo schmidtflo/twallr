8f34c62b4bb531adcb97ce69a018ded0ed75f9c1
schmidtflo <web@florian-schmidt.org>
2013-10-28 17:22:43 +0100

    Version 1.1
    - modular tweets
    - no t.co links anymore!
    - linked user, status & hashtag

--- script/script.php
+++ script/script.php
@@ -8,7 +8,7 @@ require_once ('../config.php');
 $cb = \Codebird\Codebird::getInstance();
 $cb->setToken($token, $token_secret);
 $cb->setReturnFormat(CODEBIRD_RETURNFORMAT_ARRAY);
-$data = $search.'&result_type=recent&count=20&include_entities=false&since_id='.$id_alt;
+$data = $search.'&result_type=recent&count=20&include_entities=true&since_id='.$id_alt;
 $reply = (array) $cb->search_tweets($data);
 
 function str_url_edit($string) {
@@ -27,48 +27,73 @@ foreach ($reply as $single){
 	if (is_array($single)){
 	foreach ($single as $tweet) {
 		if (isset($tweet['text'])&&$tweet['id_str']>$id_alt) {
-			setlocale(LC_ALL, 'de_DE');
-			$tweetdate_time = strtotime($tweet['created_at']);
-			$tweetdate = strftime("%H:%M", $tweetdate_time);
-			$text = str_url_edit($tweet['text']);
-			$tweet_id=$tweet['id_str'];
+			
+			if ($tweet['id_str'] > $id_neu) $id_neu = $tweet['id_str'];
 			$rt=FALSE;
-			if(isset($tweet['retweeted_status'])) $rt=TRUE;
+			if($tweet['retweeted_status']!=NULL) $rt=TRUE;
+			setlocale(LC_ALL, 'de_DE');
+			
+			$tweetdate = strftime("%H:%M", strtotime($tweet['created_at']));
+			if ($rt==TRUE) {$tweetdate_time = strtotime($tweet['retweeted_status']['created_at']);$tweetdate = strftime("%H:%M", $tweetdate_time);}
+			
+			$text = $tweet['text'];
+			if ($rt==TRUE) {$text = $tweet['retweeted_status']['text'];}
+			
+					
+			if ($rt==TRUE){foreach($tweet['retweeted_status']['entities']['urls'] as $url){
+				$replacement = '<a href="'.$url['expanded_url'].'" target="_blank">'.$url['display_url'].'</a>';
+				$text = str_replace($url['url'],$replacement,$text);
+			}}
+			else{foreach($tweet['entities']['urls'] as $url){
+				$replacement = '<a href="'.$url['expanded_url'].'" target="_blank">'.$url['display_url'].'</a>';
+				$text = str_replace($url['url'],$replacement,$text);
+			}}
+			
+			if ($rt==TRUE){foreach($tweet['retweeted_status']['entities']['user_mentions'] as $mention){
+				$replacement = '<a href="https://twitter.com/'.$mention['screen_name'].'" target="_blank">'.$mention['screen_name'].'</a>';
+				$text = str_replace($mention['screen_name'],$replacement,$text);
+			}}
+			else{foreach($tweet['entities']['user_mentions'] as $mention){
+				$replacement = '<a href="https://twitter.com/'.$mention['screen_name'].'" target="_blank">'.$mention['screen_name'].'</a>';
+				$text = str_replace($mention['screen_name'],$replacement,$text);
+			}}
+			
+			if ($rt==TRUE){foreach($tweet['retweeted_status']['entities']['hashtags'] as $hashtag){
+				$replacement = '<a href="https://twitter.com/search?q=%23'.$hashtag['text'].'" target="_blank">'.$hashtag['text'].'</a>';
+				$text = str_replace($hashtag['text'],$replacement,$text);
+			}}
+			else{foreach($tweet['entities']['hashtags'] as $hashtag){
+				$replacement = '<a href="https://twitter.com/search?q=%23'.$hashtag['text'].'" target="_blank">'.$hashtag['text'].'</a>';
+				$text = str_replace($hashtag['text'],$replacement,$text);
+			}}
 			
 			
-			if ($tweet_id > $id_neu) $id_neu = $tweet_id; 
 			
-			if ($rt==FALSE){
+			if ($rt==TRUE){$tweet_id=$tweet['retweeted_status']['id_str'];}
+			else{$tweet_id=$tweet['id_str'];}
+			
+			
+			if ($rt==TRUE){$name = $tweet['retweeted_status']['user']['name'];			
+			$screen_name = $tweet['retweeted_status']['user']['screen_name'];
+			$rt_name = $tweet['user']['screen_name'];}
+			else {$name = $tweet['user']['name'];			
+			$screen_name = $tweet['user']['screen_name'];}
+			
+			if ($rt==TRUE){$profil_image = $tweet['retweeted_status']['user']['profile_image_url'];}
+			else{$profil_image = $tweet['user']['profile_image_url'];}
+			
+			
+			
+			
 			echo"<div ";
 			echo "class=\"neu panel panel-default\" id=\"$tweet_id\"><div class=\"panel-heading\">";
-			print_r($tweet['user']['name']);
-			echo"  (@";
-			print_r($tweet['user']['screen_name']);
-			echo") | $tweetdate</div><div class=\"panel-body\"><img src=\"";
-			echo $tweet['user']['profile_image_url'];
-			echo"\"></img><p class=\"text\">$text</p></div></div>";
-			};
+			echo "$name  (@<a href=\"https://twitter.com/$screen_name\" target=\"_blank\">$screen_name</a>) | <a href=\"https://twitter.com/$screen_name/status/$tweet_id\" target=\"_blank\">$tweetdate</a> ";
+			if ($rt==TRUE){echo"| <span class=\"glyphicon glyphicon-retweet\"></span> <a href=\"https://twitter.com/$rt_name\" target=\"_blank\">$rt_name</a>";}
+			echo"</div><div class=\"panel-body\"><img src=\"$profil_image\"></img><p class=\"text\">$text</p></div></div>";
 			
-			
-			
-			if ($rt==TRUE){
-			$text = str_url_edit($tweet['retweeted_status']['text']);
-			$tweetdate_time = strtotime($tweet['retweeted_status']['created_at']);
-			$tweetdate = strftime("%H:%M", $tweetdate_time);
-			echo"<div ";
-			echo "class=\"neu panel panel-default\" id=\"$tweet_id\"><div class=\"panel-heading\">";
-			print_r($tweet['retweeted_status']['user']['name']);
-			echo"  (@";
-			print_r($tweet['retweeted_status']['user']['screen_name']);
-			echo") | $tweetdate | <span class=\"glyphicon glyphicon-retweet\"></span>  ";
-			echo" @";
-			print_r($tweet['user']['screen_name']);
-			echo"</div><div class=\"panel-body\"><img src=\"";
-			echo $tweet['retweeted_status']['user']['profile_image_url'];
-			echo"\"></img><p class=\"text\">$text</p></div></div>";
-			};
+			 
 
-		};
+			};
 	};
 	};
 
